2ae10ba2d72060b724da2de40dea011dba835fc4
Rahul <prprpandey@gmail.com>
2024-12-01 21:26:37 +0530

    que_2 solved

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+// var btn = document.querySelector('button')
+// btn.addEventListener('click',()=>{
+//      document.querySelector('p').textContent = 'Hello text is now changed'
+// })
+
+
 var btn = document.querySelector('button')
+var img1 = document.querySelector('#img1')
+var img2 = document.querySelector('#img2')
+var source;
 btn.addEventListener('click',()=>{
-     document.querySelector('p').textContent = 'Hello text is now changed'
+   source = img1.src 
+   img1.src = img2.src
+   img2.src = source
+   
 })
