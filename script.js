a5450f1de11b31aab18fd0328b989f14fbd9961b
Rahul <prprpandey@gmail.com>
2024-12-02 08:30:51 +0530

    ques_03 solved

--- script.js
+++ script.js
@@ -4,13 +4,28 @@
 // })
 
 
-var btn = document.querySelector('button')
-var img1 = document.querySelector('#img1')
-var img2 = document.querySelector('#img2')
-var source;
-btn.addEventListener('click',()=>{
-   source = img1.src 
-   img1.src = img2.src
-   img2.src = source
+// var btn = document.querySelector('button')
+// var img1 = document.querySelector('#img1')
+// var img2 = document.querySelector('#img2')
+// var source;
+// btn.addEventListener('click',()=>{
+//    source = img1.src 
+//    img1.src = img2.src
+//    img2.src = source
    
+// })
+
+var form = document.querySelector('form')
+var inps = document.querySelectorAll('input[type="text"]')
+var h4 = document.querySelector('h4')
+
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  for(var i=0;i<inps.length;i++){
+   if(inps[i].value ===' '){
+       h4.textContent = "Error some field are blanck";
+       h4.style.color = 'red'
+       break;
+   }
+  }
 })
