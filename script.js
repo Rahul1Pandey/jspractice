3b2888ccc73f0a531fac226d766ba5d7d13b6c6c
Rahul <prprpandey@gmail.com>
2024-12-02 08:52:55 +0530

    que_04 solved

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-// var btn = document.querySelector('button')
-// btn.addEventListener('click',()=>{
-//      document.querySelector('p').textContent = 'Hello text is now changed'
-// })
+var add = document.querySelector('#add')
+var remove = document.querySelector('#remove')
+var inp = document.querySelector('input')
+var ul = document.querySelector('ul')
 
-
-// var btn = document.querySelector('button')
-// var img1 = document.querySelector('#img1')
-// var img2 = document.querySelector('#img2')
-// var source;
-// btn.addEventListener('click',()=>{
-//    source = img1.src 
-//    img1.src = img2.src
-//    img2.src = source
-   
-// })
-
-var form = document.querySelector('form')
-var inps = document.querySelectorAll('input[type="text"]')
-var h4 = document.querySelector('h4')
-
-form.addEventListener('submit',(e)=>{
-  e.preventDefault();
-  for(var i=0;i<inps.length;i++){
-   if(inps[i].value ===' '){
-       h4.textContent = "Error some field are blanck";
-       h4.style.color = 'red'
-       break;
-   }
-  }
+var li;
+add.addEventListener('click',()=>{
+    if(inp.value===" "){}
+    else{
+       li = document.createElement('li')
+      li.textContent = inp.value;
+      ul.appendChild(li)
+      inp.value = ""
+    }
 })
+
+remove.addEventListener('click',()=>{
+     ul.removeChild(li);
+   }
+)
