7b42e0877c738cbf647aaa88dfc1bf2278a5e26b
Rahul <prprpandey@gmail.com>
2024-12-02 09:25:53 +0530

    ques_06 solved

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-var strt = document.querySelector('#strt')
-var stop = document.querySelector('#stop')
-var h3 = document.querySelector('h3')
+var home = document.querySelector('#home')
+var about = document.querySelector('#about')
+var contact = document.querySelector('#contact')
 
-var inp;
-strt.addEventListener('click',()=>{
-   var count = 0;
-   inp = setInterval(()=>{
-      h3.textContent = count;
-      count++
-   },1000)
-}) 
 
-stop.addEventListener('click',()=>{
-  clearInterval(inp)
+var homeText = document.querySelector('#homeText')
+var aboutText = document.querySelector('#aboutText')
+var contactText = document.querySelector('#contactText')
+
+// homeText.style.display = "block";
+// homeText.style.width = "50%";
+
+home.addEventListener('click',function(){
+    removeTex();
+    homeText.style.display = "block";
+    homeText.style.width = "50%";
 })
+about.addEventListener('click',function(){
+    removeTex();
+  aboutText.style.display = "block";
+  aboutText.style.width = "50%";
+})
+contact.addEventListener('click',function(){
+    removeTex();
+    contactText.style.display = "block";
+    contactText.style.width = "50%";
+})
+
+function removeTex(){
+    document.querySelectorAll('h3').forEach(function(h3){
+        h3.style.display = "none";
+    })
+}
